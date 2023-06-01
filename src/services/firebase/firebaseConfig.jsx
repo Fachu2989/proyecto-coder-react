218b60ad96cbb0f43b43239c441bb9ba712d66ca
Fachu2989 <Fachu290589@gmail.com>
2023-06-01 17:48:44 -0300

    Update firebaseConfig.jsx

--- src/services/firebase/firebaseConfig.jsx
+++ src/services/firebase/firebaseConfig.jsx
@@ -8,7 +8,7 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_projectId,
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  // appId: process.env.REACT_APP_appId,
+  appId: process.env.REACT_APP_appId,
   // apiKey: "AIzaSyC9_WP7_aD8rI_vMqcIZAFvIZZU_QsCXJY",
   // authDomain: "proyecto-coder-24ce1.firebaseapp.com",
   // projectId: "proyecto-coder-24ce1",
